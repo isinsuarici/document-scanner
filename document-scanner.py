fcd3e9d791ef4199c4c3e098e0e072f58e9f9071
isinsuarici <isinsuu98@hotmail.com>
2023-02-04 05:52:20 +0300

    Imports and base of Tk gui added.

--- document-scanner.py
+++ document-scanner.py
@@ -5,13 +5,45 @@ Created on Sat Feb  4 03:00:11 2023
 @author: Isinsu
 """
 import cv2
+import numpy as np
+import glob,os
+import thinker as tk
+from thinker import *
+
+import time, datetime
+from urllib.request import urlopen
+from pathlib import Path
+from skimage.filters import threshold_local
+
+from PIL import ImageTk
+import PIL
+from PIL import ImageDraw, ImageFont
+from PIL import Image
+
+
+
+window = Tk()
+window.configure(background="white")
+window.title("Document Scanner")
+width = window.winfo_screenwidth()
+height = window.winfo_screenheight()
+window.geometry(f'{width}x{height}')
+
+window.resizable(0, 0)
+launch = False
+
+w = 400 ;h = 600
+size = (w, h)
+
+
+
 
 def main():
     img = cv2.imread("C:/Users/Isinsu/Desktop/PROJECTS/document-scanner/imgs/img1.png",0)
-    while True:
-        cv2.imshow('img', img)
-        if cv2.waitKey(20) & 0xFF == 27:  # press esc to exit
-            break
+    
+    cv2.imshow('img', img)
+    
+    cv2.waitKey(0)    
     cv2.destroyAllWindows()
     
 
